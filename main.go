4bfbca75228405640acf9a0c328ff486274e7e97
Dmitriy Bratchikov <yunglocokid@gmail.com>
2022-03-31 19:00:48 +0700

    Added string to BubbleSort

--- main.go
+++ main.go
@@ -10,5 +10,6 @@ func main() {
 	a := []int{5, 7, 4, 5, 8, 3, 23, 53, 211, 2, 8, -4, 3}
 	fmt.Println("Hello from wizzlix algorithms collection!")
 
-	fmt.Println(sorts.BubbleSort(a))
+	fmt.Println("Buble Sort :", sorts.BubbleSort(a))
+
 }
