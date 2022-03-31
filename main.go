f35892d2c7b7000ed489797d620d13e13dc3fc50
Dmitriy Bratchikov <yunglocokid@gmail.com>
2022-03-31 18:34:08 +0700

    Added bubbleSort()

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"./sorts"
+)
 
 func main() {
+	a := []int{5, 7, 4, 5, 8, 3, 23, 53, 211, 2, 8, -4, 3}
 	fmt.Println("Hello from wizzlix algorithms collection!")
+
+	fmt.Println(sorts.BubbleSort(a))
 }
