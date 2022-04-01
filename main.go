c13d71355c23261929b2849f2e39b42f8842da93
Dmitriy Bratchikov <bratchikov9952@mail.ru>
2022-04-01 10:09:45 +0700

    add docs

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+/* Импорт пакета `fmt` и пакета `sorts`*/
 import (
 	"fmt"
 
@@ -7,9 +8,11 @@ import (
 )
 
 func main() {
+	/* Короткий способ объявить и инициализировать срез целых чисел. */
 	a := []int{5, 7, 4, 5, 8, 3, 23, 53, 211, 2, 8, -4, 3}
 	fmt.Println("Hello from wizzlix algorithms collection!")
 
+	/* Вызов функции из другого пакета. */
 	fmt.Println("Buble Sort :", sorts.BubbleSort(a))
 
 }
